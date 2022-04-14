164e931734a951e1282cead5ae3c6d832a98e5bb
Andy Merrill <andy@andymerrill.tech>
2022-04-13 22:55:36 -0500

    made printer print

--- pages/api/print/[id].ts
+++ pages/api/print/[id].ts
@@ -3,6 +3,8 @@ import { getPrinters, printDirect } from "printer";
 import QRCode from "qrcode";
 import { supabase } from "../../../utils/supabase";
 import { Asset } from "../../../utils/types";
+import * as fs from "fs";
+import {execSync} from "child_process";
 
 export default async function handler(
   req: NextApiRequest,
@@ -35,19 +37,7 @@ export default async function handler(
     return;
   }
 
-  const qrCode = await QRCode.toBuffer(id.toString(), { type: "png" });
-  console.log(qrCode);
-  printDirect({
-    data: qrCode,
-    type: "RAW",
-    printer: process.env.PRINTER_NAME,
-    error: (e) => {
-      console.error(e);
-      res.status(500).json({ error: e.message });
-    },
-    success: (jobId) => {
-      console.log(`Printed QR code for asset ${id} with job id ${jobId}`);
-      res.status(200).json({ success: true });
-    },
-  });
+  const qrCode = await QRCode.toFile("temp.png", id.toString(), { type: "png" });
+  const fuck = execSync("/usr/bin/lp -d SII_SLP100 -o media=custom_83.31x24.13mm_83.31x24.13mm -o sides=one-sided temp.png");
+  res.status(200).json({ success: true });
 }
